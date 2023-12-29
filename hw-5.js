32346182db3023818d45a6e07433b326dc7da0e1
DimaGAV <gav_gav82@mail.ru>
2023-12-29 22:09:55 +0300

    осталось прикрутить

---
+++ hw-5.js
@@ -0,0 +1,133 @@
+//Задание 1
+function min(a, b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+min(5, 10);
+
+//Задание 2
+function parity() {
+    let yourNumber = prompt("Пожалуйста, введите любое целое число");
+    if (isNaN(yourNumber)) {
+        console.log("Это не число");
+    } else if (yourNumber % 2 === 0) {
+        return "Чётное число";
+    } else {
+        return "Нечётное число";
+    }
+}
+
+parity();
+
+//Задание 3.1
+let square1 = (e) => {
+    let sq = e ** 2;
+    console.log(`Квадрат этого числа равен ${sq}`);
+}
+
+square1(e = prompt("Пожалуйста, введите любое число"));
+
+//Задание 3.2
+let square2 = (g) => {
+    let result = g ** 2;
+    return result;
+}
+
+square2(g = prompt("Пожалуйста, введите любое число"));
+
+//Задание 4
+function greeting() {
+    let clientAge = Number(prompt("Сколько Вам лет?"));
+    if (isNaN(clientAge)) {
+        console.log("Ввели не число");
+    }
+    else if (clientAge < 0) {
+        console.log("Вы ввели неправильное значение");
+    }
+    else if (clientAge >= 0 && clientAge <= 12) {
+        console.log("Привет, друг!");
+    }
+    else if (clientAge >= 13) {
+        console.log("Добро пожаловать!");
+    }
+}
+
+greeting();
+
+//Задание 5
+function mult1(num1, num2) {
+    num1 = Number(prompt("Введите первое число"));
+    num2 = Number(prompt("Введите второе число"));
+    if (isNaN(num1) || isNaN(num2)) {
+        return ("Одно или оба значения не являются числом");
+    }
+    else {
+        return num1 * num2;
+    }
+}
+
+mult1();
+
+//Задание 6
+function degree(userAnswer) {
+    userAnswer = Number(prompt("Введите число"));
+    if (isNaN(userAnswer)) {
+        console.log("Переданный параметр не является числом");
+    }
+    else {
+        let result1 = Math.pow(userAnswer, 3);
+        return `n в кубе равняется ${result1}`;
+    }
+}
+
+degree();
+
+//Задание 7
+const circle1 = {
+    radius: 5,
+    getArea() {
+        return `${this.radius}` ** 2 * Math.PI;
+    },
+    getPerimeter() {
+        return 2 * `${this.radius}` * Math.PI;
+    }
+}
+
+console.log(circle1.getArea());
+console.log(circle1.getPerimeter());
+
+const circle2 = {
+    radius: 8,
+    getArea() {
+        return `${this.radius}` ** 2 * Math.PI;
+    },
+    getPerimeter() {
+        return 2 * `${this.radius}` * Math.PI;
+    }
+}
+
+console.log(circle2.getArea());
+console.log(circle2.getPerimeter());
+
+//Задание 8
+function months(monthNumber) {
+    monthNumber = Number(prompt('Напиши номер месяца'));
+
+    if ((monthNumber > 0 && monthNumber < 3) || monthNumber === 12) {
+        return 'Зима';
+    } else if (monthNumber >= 3 && monthNumber <= 5) {
+        return 'Весна';
+    } else if (monthNumber >= 6 && monthNumber <= 8) {
+        return 'Лето';
+    } else if (monthNumber >= 9 && monthNumber <= 11) {
+        return 'Осень';
+    } else {
+        console.log('Введен некорректный номер месяца');
+    }
+}
+
+months();
